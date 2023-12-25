326410c80d34410b314f541da062fd13f4510d84
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2023-12-25 22:20:34 +0530

    changing absolute path to relative for image rendering

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -4,67 +4,67 @@ const Skill = () => {
   const skills = [
     {
       name: "JAVA",
-      imgSrc: "../../../public/java.png",
+      imgSrc: "/public/java.png",
       width: "80px",
     },
     {
       name: "javaScript",
-      imgSrc: "../../../public/js-logo.png",
+      imgSrc: "/public/js-logo.png",
       width: "80px",
     },
     {
       name: "HTML",
-      imgSrc: "../../../public/html.png",
+      imgSrc: "/public/html.png",
       width: "80px",
     },
     {
       name: "CSS",
-      imgSrc: "../../../public/css-logo.png",
+      imgSrc: "/public/css-logo.png",
       width: "80px",
     },
     {
       name: "React",
-      imgSrc: "../../../public/react-logo.png",
+      imgSrc: "/public/react-logo.png",
       width: "80px",
     },
     {
       name: "nodeJS",
-      imgSrc: "../../../public/nodeJS.png",
+      imgSrc: "/public/nodeJS.png",
       width: "80px",
     },
     {
       name: "mongoDB",
-      imgSrc: "../../../public/mongodb.jpeg",
+      imgSrc: "/public/mongodb.jpeg",
       width: "80px",
     },
     {
       name: "SQL",
-      imgSrc: "../../../public/sql.png",
+      imgSrc: "/public/sql.png",
       width: "80px",
     },
     {
       name: "Styled Components",
-      imgSrc: "../../../public/styledComponent.png",
+      imgSrc: "/public/styledComponent.png",
       width: "80px",
     },
     {
       name: "Redux",
-      imgSrc: "../../../public/redux.png",
+      imgSrc: "/public/redux.png",
       width: "80px",
     },
     {
       name: "DSA",
-      imgSrc: "../../../public/dsa.jpg",
+      imgSrc: "/public/dsa.jpg",
       width: "80px",
     },
     {
       name: "SaaS",
-      imgSrc: "../../../public/saas.png",
+      imgSrc: "/public/saas.png",
       width: "80px",
     },
     {
       name: "JDBC",
-      imgSrc: "../../../public/jdbc.jpeg",
+      imgSrc: "/public/jdbc.jpeg",
       width: "80px",
     },
   ];
