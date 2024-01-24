e8760306a75d5313cecd5761aea8598138eed519
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2024-01-24 14:17:04 +0530

    adding button link

---
+++ src/components/Button/GithubButton.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+const ContainerGit = styled.div``;
+const Anchor = styled.a`
+  background-color: #f179a7;
+  padding: 1rem 1.3rem;
+  text-decoration: none;
+  color: white;
+  font-size: 1.5rem;
+  text-align: center;
+  cursor: pointer;
+  border-radius: 3px;
+`;
+const GithubButton = () => {
+  return (
+    <ContainerGit>
+      <Anchor>Github</Anchor>
+    </ContainerGit>
+  );
+};
+
+export default GithubButton;
