bc9f58beb87a512d64804cf4649ffb8e4c2f03a9
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2024-02-10 20:43:48 +0530

    adding mobile navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import Education from "./components/Education/Education";
 import Footer from "./components/Footer/FooterSec";
 import Pricing from "./components/Pricing/pricing";
 import Contact from "./components/Contact/Contact";
-// import Navbar from "./components/Navbar/Navbar";
+
 import Wave from "./components/Wave/Wave";
 import Scroll from "./components/Scroll/Scroll";
-// import Navbar1 from "./components/Navbar1/Navbar1";
+import Navbar1 from "./components/Navbar1/Navbar1";
 const App = () => {
   return (
     <div>
-      {/* <Navbar1 /> */}
+      <Navbar1 />
       <Hero />
       <About />
       <Skill />
