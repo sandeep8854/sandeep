50bdd58672a8c60f2da7155b2bdccb53be6ae1c5
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2024-07-15 16:43:01 +0530

    modified some and added picture

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import { TfiInfoAlt } from "react-icons/tfi";
 import "./about.css";
 import Leaf from "../Leaf/Leaf.jsx";
+import AboutImage from "../../../public/about.png";
 
 const About = () => {
   return (
@@ -12,15 +13,19 @@ const About = () => {
         <h1 className="section-title about-title">Little About Me ~</h1>
       </div>
       <Leaf />
-
-      <div className="page-container">
-        <p className="about-content">
-          Enthusiastic, goal oriented & dedicated frontend developer with
-          experience building and maintaining responsive websites controlling
-          end to end application development. Looking forward to explore more
-          and gain knowledge to enhance my skills & achieve desired results for
-          company's goals.
-        </p>
+      <div className="imageAndAbout">
+        <div className="page-container">
+          <p className="about-content">
+            Enthusiastic, goal oriented & dedicated frontend developer with
+            experience building and maintaining responsive websites controlling
+            end to end application development. Looking forward to explore more
+            and gain knowledge to enhance my skills & achieve desired results
+            for company's goals.
+          </p>
+        </div>
+        <div className="image-container">
+          <img src={AboutImage} alt="profile image" />
+        </div>
       </div>
     </section>
   );
