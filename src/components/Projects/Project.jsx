ee3c38c112a5db8196c51ef9f94277b1b42aa6b0
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2024-08-29 14:55:32 +0530

    added

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -7,7 +7,7 @@ import Leaf from "../Leaf/Leaf.jsx";
 const Project = () => {
   const projectData = [
     {
-      image: "../../../public/developer-portfolio_1.jpg",
+      image: "../../../public/developer-portfolio_2_3.jpg",
       name: "Developer Portfolio",
       desc: "Software Developer Portfolio Template build with react.js and styled component that helps you showcase our work and skills as a software developer.",
       gitHub: "https://github.com/sandeep8854/sandeep",
@@ -15,7 +15,7 @@ const Project = () => {
       target: "_blank",
     },
     {
-      image: "../../../public/pizza_2.jpg",
+      image: "../../../public/pizza_2_3.jpg",
       name: "Fast React Pizza Co.",
       desc: "Consumers can order their pizzas by calling the single Happiness Hotline number 18002081234 OR order online at fastreactpizza.com online. and will be recieved 24/7",
       gitHub: "https://github.com/sandeep8854/fast-react-pizza",
@@ -23,7 +23,7 @@ const Project = () => {
       target: "_blank",
     },
     {
-      image: "../../../public/travel-list_2.jpg",
+      image: "../../../public/travel-list_2_3.jpg",
       name: "Travel List",
       desc: "In this project user can add the items, delete the items and also and sort the item based on descrieption, input order and packed status.",
       gitHub: "https://github.com/sandeep8854/travel-list",
