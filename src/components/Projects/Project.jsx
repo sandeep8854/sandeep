72360556da5eb6d12390a47f676c497cd7729031
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2024-08-31 21:39:14 +0530

    added

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -6,6 +6,7 @@ import Leaf from "../Leaf/Leaf.jsx";
 import Img1 from "../../../public/developer-portfolio_2_3.jpg";
 import Img2 from "../../../public/pizza_2_3.jpg";
 import Img3 from "../../../public/travel-list_2_3.jpg";
+import Skills from "../Button/Skills.jsx";
 
 const imageArray = [Img1, Img2, Img3];
 
@@ -18,14 +19,16 @@ const Project = () => {
       gitHub: "https://github.com/sandeep8854/sandeep",
       linkDemo: "https://sandeepprajapati.vercel.app/",
       target: "_blank",
+      technologies: ["Html", "css", "styledComponent", "reactJS"],
     },
     {
       image: "../../../public/pizza_2_3.jpg",
       name: "Fast React Pizza Co.",
-      desc: "Consumers can order their pizzas by calling the single Happiness Hotline number 18002081234 OR order online at fastreactpizza.com online. and will be recieved 24/7",
+      desc: "User can order their pizzas by calling single Happiness Hotline 18002081234 OR order online at fastreactpizza.com and will be recieved 24/7",
       gitHub: "https://github.com/sandeep8854/fast-react-pizza",
       linkDemo: "https://fast-react-pizza-sandeep.vercel.app/",
       target: "_blank",
+      technologies: ["Html", "css", "Tailwind CSS", "reactJS"],
     },
     {
       image: "../../../public/travel-list_2_3.jpg",
@@ -34,6 +37,7 @@ const Project = () => {
       gitHub: "https://github.com/sandeep8854/travel-list",
       linkDemo: "https://travel-list-sandeep.vercel.app/",
       target: "_blank",
+      technologies: ["Html", "CSS", "reactJS"],
     },
   ];
   return (
@@ -64,6 +68,9 @@ const Project = () => {
                   <p className="project-text" key={index}>
                     {ele.desc}
                   </p>
+                  <div className="skill-tag">
+                    <Skills />
+                  </div>
                   <div className="button-container">
                     <>
                       <a href={ele.gitHub} target={ele.target}>
